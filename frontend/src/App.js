e7959832801ee1af8eb13095eac43e8d8690d05d
rahil khan <khanrahil97591@gmail.com>
2020-10-07 15:56:27 +0530

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './App.css';
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      Welcome to ProShop
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1> Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 

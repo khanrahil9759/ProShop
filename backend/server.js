ad484acdb004dc0bffd1885044ec1d2fb60bc012
rahil khan <khanrahil97591@gmail.com>
2020-10-07 21:56:09 +0530

    database setup done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
-dotenv.config()
 
 app.get('/', (req, res) => {
     res.send('API is running')
@@ -20,4 +25,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server is in ${process.env.NODE_ENV} running on port ${PORT}`))
+app.listen(PORT, console.log(`Server is in ${process.env.NODE_ENV} running on port ${PORT}`.yellow.bold))
